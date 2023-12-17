747217654a8e489fc8bf3cf4b3bb4fe32478242e
Rodolpho Alves <rodolpho_alves@trimble.com>
2023-12-17 16:38:59 -0300

    feat: describe messages and users

---
+++ src/main/kotlin/br/com/ardc/springsandbox/domain/Domain.kt
@@ -0,0 +1,17 @@
+package br.com.ardc.springsandbox.domain
+
+/**
+ * A representation of an User for messaging needs.
+ * @param userName the userName (for logins)
+ * @param displayName the display name (for displaying)
+ */
+data class User(val userName: String, val displayName: String?)
+
+/**
+ * A representation of a Message sent between two users.
+ * @param sender the user sending the message
+ * @param recipient the user that should receive the message
+ * @param content the contents of the string itself, in a string format
+ * @param isRead flag indicating if the message was read by the recipient or not, once this is true the message can no longer be deleted or editted
+ */
+data class Message(val sender: User, val recipient: User, val content: String, val isRead: Boolean)
---
+++ src/test/kotlin/br/com/ardc/springsandbox/domain/DomainTests.kt
@@ -0,0 +1,72 @@
+package br.com.ardc.springsandbox.domain
+
+import org.assertj.core.api.Assertions.*
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.KotlinModule
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+
+class DomainTests {
+    private val expectedUsername = "ralves"
+    private val expectedDisplayName = "Rodolpho Alves"
+    private val expectedUserJson = """{"userName":"$expectedUsername","displayName":"$expectedDisplayName"}"""
+    private val expectedMessageContent = "I like turtles"
+    private val expectedUserObject = User(expectedUsername, expectedDisplayName)
+    private val expectedMessageReadStatus = false
+    private val expectedMessageJson = """{"sender":$expectedUserJson,"recipient":$expectedUserJson,"content":"$expectedMessageContent","isRead":$expectedMessageReadStatus}"""
+    private val expectedMessageObject = Message(expectedUserObject, expectedUserObject, expectedMessageContent, expectedMessageReadStatus)
+
+    @Nested
+    inner class UserTests {
+        @Test
+        fun `Given a valid JSON, When serialized, Then an User instance is retrieved`(): Unit {
+            // Arrange
+            val objectMapper = ObjectMapper().registerModules(KotlinModule.Builder().build())
+
+            // Act
+            val got = objectMapper.readValue(expectedUserJson, User::class.java)
+
+            // Assert
+            assertThat(got).isEqualTo(expectedUserObject)
+        }
+
+        @Test
+        fun `Given a valid Object, When serialized, Then a JSON is retrieved`(): Unit {
+            // arrange
+            val objectMapper = ObjectMapper().registerModules(KotlinModule.Builder().build())
+
+            // act
+            val got = objectMapper.writeValueAsString(expectedUserObject)
+
+            // assert
+            assertThat(got).isEqualTo(expectedUserJson)
+        }
+    }
+
+    @Nested
+    inner class MessageTests {
+        @Test
+        fun `Given a valid JSON, When Serialized, Then a Message instance is recovered`(): Unit {
+            // arrange
+            val objectMapper = ObjectMapper().registerModules(KotlinModule.Builder().build())
+
+            // act
+            val got = objectMapper.readValue(expectedMessageJson, Message::class.java)
+
+            // assert
+            assertThat(got).isEqualTo(expectedMessageObject)
+        }
+
+        @Test
+        fun `Given a valid Instance, When Serialized, Then a JSON is returned`(): Unit {
+            // arrange
+            val objectMapper = ObjectMapper().registerModules(KotlinModule.Builder().build())
+
+            // act
+            val got = objectMapper.writeValueAsString(expectedMessageObject)
+
+            // assert
+            assertThat(got).isEqualTo(expectedMessageJson)
+        }
+    }
+}
